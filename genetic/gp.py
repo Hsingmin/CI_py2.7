ef2906755d498a054beec20a99c4784351816cce
Alfred Lee <alfred_bit@sina.cn>
2017-12-16 21:01:19 +0800

    /genetic/gp.py edited to be executed

--- genetic/gp.py
+++ genetic/gp.py
@@ -153,7 +153,83 @@ def getrankfunction(dataset):
 		return scores
 	return rankfunction
 
+def gridgame(p):
+	max = (3, 3)
+	lastmove = [-1, -1]
+	location = [[randint(0, max[0]), randint(0, max[1])]]
+	location.append([(location[0][0]+2)%4, (location[0][1]+2)%4])
+	for o in range(50):
+		for i in range(2):
+			locs = location[i][:]+location[1-i][:]
+			locs.append(lastmove[i])
+			move = p[i].evaluate(locs)%4
 
+			if lastmove[i] == move:
+				return 1-i
+			if move == 0:
+				location[i][0] -= 1
+				if location[i][0] < 0:
+					location[i][0] = 0
+			if move == 1:
+				location[i][0] += 1
+				if location[i][0] > max[0]:
+					location[i][0] = max[0]
+			if move == 2:
+				location[i][1] -= 1
+				if location[i][1] < 0:
+					location[i][1] = 0
+			if move == 3:
+				location[i][1] += 1
+				if location[i][1] > max[1]:
+					location[i][1] = max[1]
+
+			if location[i] == location[1-i]:
+				return i
+	return -1
+
+def tournament(pl):
+	losses = [0 for p in pl]
+	for i in range(len(pl)):
+		for j in range(len(pl)):
+			if i == j:
+				continue
+			winner = gridgame([pl[i], pl[j]])
+
+			if winner == 0:
+				losses[j] += 2
+			elif winner == 1:
+				losses[i] += 2
+			elif winner == -1:
+				losses[i] += 1
+				losses[i] += 1
+				pass
+	z = zip(losses, pl)
+	z.sort()
+	return z
+
+class humanplayer:
+	def evaluate(self, board):
+		me = tuple(board[0:2])
+		others = [tuple(board[x:x+2]) for x in range(2, len(board)-1, 2)]
+
+		for i in range(4):
+			for j in range(4):
+				if (i,j) == me:
+					print 'O',
+				elif (i,j) in others:
+					print 'X',
+				else:
+					print '.',
+			print
+
+		print 'Your last move was %d' % board[len(board)-1]
+		print ' 0'
+		print '2 3'
+		print ' 1'
+		print 'Enter move: ',
+
+		move = int(raw_input())
+		return move
 
 
 
